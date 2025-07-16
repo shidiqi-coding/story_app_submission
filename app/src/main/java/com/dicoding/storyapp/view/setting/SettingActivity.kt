ac647d025c2e6271d4cd18c749bcd9aff7901197
shidiqi-coding <shidiqiihsan@gmail.com>
2025-07-16 20:24:21 +0700

    memperbaiki fitur login

---
+++ app/src/main/java/com/dicoding/storyapp/view/setting/SettingActivity.kt
@@ -0,0 +1,20 @@
+package com.dicoding.storyapp.view.setting
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.storyapp.R
+
+class SettingActivity : AppCompatActivity() {
+
+//    private lateinit var viewModel: SettingViewModel
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        setContentView(R.layout.activity_setting)
+//        viewModel.
+//    }
+}
